a11e69e4e3bf013594d9ae482f75a12dc292fdcf
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-25 12:04:15 -0500

    [Update] - Create main object. // Add to IndexDB // Show submit results in the DOM.

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -23,15 +23,25 @@
 
     //Leer todos los inputs.
     const nombre = document.querySelector('#nombre').value;
-    const email = document.querySelector('#email').value;
+    const correo = document.querySelector('#email').value;
     const telefono = document.querySelector('#telefono').value;
     const empresa = document.querySelector('#empresa').value;
 
-    if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+    if (nombre === '' || correo === '' || telefono === '' || empresa === '') {
       imprimirAlerta('Todos los campos son obligatorios.', 'error');
       return;
     };
 
+    //Crear un objeto con la informacion del formulario.
+    const cliente = {
+      nombre,
+      correo,
+      telefono,
+      empresa,
+    };
+    cliente.id = Date.now();
+
+    crearCliente(cliente);
   };
 
   function imprimirAlerta(mensaje, tipo) {
@@ -52,4 +62,23 @@
       }, 3000);
     }
   };
+
+  function crearCliente(cliente) {
+    const transaction = DB.transaction(['crm'], 'readwrite');
+    const objectStore = transaction.objectStore('crm');
+    
+    objectStore.add(cliente);
+    transaction.onerror = function () {
+      imprimirAlerta('Hubo un error. ⚠', 'error');
+    };
+
+    transaction.oncomplete = () => {
+      imprimirAlerta('¡Cliente agregado! ✌🏻');
+      formulario.reset();
+      setTimeout(() => {
+        window.location.href = 'index.html';
+      }, 3000);
+    };
+
+  }
 })();
